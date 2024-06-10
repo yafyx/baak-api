84ef846604d8570f78da3186541b04ef098dcfcb
fyx <68360939+yafyx@users.noreply.github.com>
2024-06-11 00:34:54 +0700

    Refactor main.go to use a switch statement for handling different endpoints

--- app/main.go
+++ app/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/yafyx/baak-api/config"
 	"github.com/yafyx/baak-api/handlers"
 )
 
-func main() {
-	http.HandleFunc("/jadwal/", handlers.HandlerJadwal)
-	http.HandleFunc("/kalender", handlers.HandlerKegiatan)
-	http.HandleFunc("/kelasbaru/", handlers.HandlerMahasiswa)
-
-	port := config.GlobalEnv.HTTPPort
-	log.Printf("Server running on port %d", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+func Handler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/jadwal/":
+		handlers.HandlerJadwal(w, r)
+	case "/kalender":
+		handlers.HandlerKegiatan(w, r)
+	case "/kelasbaru/":
+		handlers.HandlerMahasiswa(w, r)
+	default:
+		http.Error(w, "404 not found.", http.StatusNotFound)
+	}
 }
+
+// func main() {
+// 	http.HandleFunc("/jadwal/", handlers.HandlerJadwal)
+// 	http.HandleFunc("/kalender", handlers.HandlerKegiatan)
+// 	http.HandleFunc("/kelasbaru/", handlers.HandlerMahasiswa)
+
+// 	port := config.GlobalEnv.HTTPPort
+// 	log.Printf("Server running on port %d", port)
+// 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+// }
